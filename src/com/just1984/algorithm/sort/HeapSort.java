9ce502ab449ed7ba2905f7a50fd38ecd697b2234
JUST1984 <yixiezi1994@gmail.com>
2018-12-24 00:00:33 +0800

    heap sort standard

--- src/com/just1984/algorithm/sort/HeapSort.java
+++ src/com/just1984/algorithm/sort/HeapSort.java
@@ -1,6 +1,7 @@
 package com.just1984.algorithm.sort;
 
 import com.just1984.algorithm.heap.MaxHeap;
+import com.just1984.algorithm.util.SortUtil;
 
 public class HeapSort extends AbstractSort {
 
@@ -10,9 +11,28 @@ public class HeapSort extends AbstractSort {
 
     @Override
     protected void sortArray(int[] array) {
-        MaxHeap maxHeap = new MaxHeap(array);
-        for (int i = array.length - 1; i >= 0; i--) {
-            array[i] = maxHeap.extractMax();
+        int len = array.length;
+        // heapify
+        for (int i = (len - 1) / 2; i >= 0; i--) {
+            shiftDown(array, len, i);
+        }
+        for (int i = len- 1; i > 1; i--) {
+            SortUtil.swap(array, 0, i);
+            shiftDown(array, i, 0);
+        }
+    }
+
+    private void shiftDown(int[] array,int n, int k) {
+        while (k * 2 + 1 < n) {
+            int j = k * 2 + 1;
+            if (j + 1 < n && array[j + 1] > array[j]) {
+                j++;
+            }
+            if (array[k] >= array[j]) {
+                break;
+            }
+            SortUtil.swap(array, k, j);
+            k = j;
         }
     }
 
